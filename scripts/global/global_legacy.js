0add2afb6c77c259ea10dc73cb221aa8f544120f
vOtherWworks <37635831+vOtherWworks@users.noreply.github.com>
2018-04-20 20:39:49 +0300

    Update global_legacy.js

--- scripts/global/global_legacy.js
+++ scripts/global/global_legacy.js
@@ -23,23 +23,4 @@
     document.addEventListener('DOMContentLoaded', browseHappy(), {once: true});
 })();
 //───────────────────────────────────browseHappy
-
-
-
-//───────────────────────────────────Creator
-(function() {
-    var projectText = '\r\n\r\n\r\n\r\n%1$cName:   %2$cFreeCodeCamp%1$c@1.0.0\r\n%1$cDate:   %3$c20.12.2017\r\n%1$cLast Modified:   %3$c11.01.2018\r\n\r\n%1$cMy site:   %3$chttps://github.com/Vastted\r\n%1$cCreator:   %3$cAndrey Bondarenko | Vastted\r\n\r\n\r\n\r\n';
-
-    var projectStyles = [
-        '',
-        'font-size: 12px; text-transform: uppercase; font-family: Helvetica; font-weight: 800; line-height: 1.4em;',
-        'font-size: 12px; text-transform: none; font-family: Helvetica; font-weight: 100; line-height: 1.4em;'
-    ];
-    
-
-    console.group('Creator:');
-    console.log(projectText, projectStyles[0], projectStyles[1], projectStyles[2]);
-    console.groupEnd('Creator:');
-})();
-//───────────────────────────────────Creator
 //# sourceMappingURL=_global_legacy.js.map
